ebfe279d66d0b2665240813ac57d09c647a4a354
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-27 23:16:33 +0200

    chore: init thumbs and constants

---
+++ src/assets/thumbnails/index.ts
@@ -0,0 +1,158 @@
+// Lofi café
+export { default as cafeInPreview } from "./cafe-in.png";
+export { default as cafeOutPreview } from "./cafe-out.png";
+export { default as cafeThumb } from "./cafe-set.png";
+
+// Forest House
+export { default as forestInsidePreview } from "./forest-inside.jpg";
+export { default as forestOutsidePreview } from "./forest-outside.jpg";
+export { default as forestThumb } from "./forest-set.png";
+
+// Summer Days
+export { default as summerOutPreview } from "./summer-out.png";
+export { default as summerPreview } from "./summer.png";
+export { default as summerThumb } from "./summer-set.png";
+
+// Van Life
+export { default as vanOutPreview } from "./van-out.png";
+export { default as vanPreview } from "./van.png";
+export { default as vanThumb } from "./van-set.png";
+
+// Ocean Tales
+export { default as oceanPreview } from "./ocean-in.png";
+export { default as oceanOutPreview } from "./o9cean-out.png";
+export { default as oceanThumb } from "./ocean-set.png";
+
+// Chill Vibes
+export { default as chillVibes } from "./chill.png";
+export { default as livingRoom } from "./living-room.png";
+export { default as bedRoom } from "./bed-room.png";
+
+// Lofi Desk
+export { default as lofi_desk } from "./lofi_desk.png";
+export { default as studyCity } from "./city_study.png";
+export { default as studyBeach } from "./beach_study.png";
+export { default as studyMountain } from "./snow_study.png";
+
+// Northern Lights
+export { default as cottage } from "./cottage.png";
+export { default as cottageIn } from "./cottage-in.png";
+export { default as cottageOut } from "./cottage-ext.png";
+
+// Book Cafe
+export { default as bookCafe } from "./book-cafe-preview.png";
+export { default as bookOut } from "./book_cafe_preview_out.png";
+export { default as bookIn } from "./book_cafe_preview_in.png";
+
+// Kyoto
+export { default as kyoto } from "./kyoto.png";
+export { default as kyotoPark } from "./kyotoPark.png";
+export { default as kyotoStreet } from "./kyotoStreet.png";
+
+// Am I Dreaming?
+export { default as dreamingScene } from "./dreamin.png";
+export { default as spacePW } from "./space-pw.png";
+export { default as underwaterPW } from "./underwater-pw.png";
+
+// Honolulu
+export { default as honoluluIn } from "./honolulu_in.png";
+export { default as honoluluOut } from "./honolulu_out.png";
+export { default as honoluluPW } from "./honolulu-pw.png";
+
+// Train Journey
+export { default as trainJourney } from "./train-journey.png";
+export { default as trainJourneyCity } from "./train-journey-city.png";
+export { default as trainJourneyCountry } from "./train-journey-country.png";
+
+// New York
+export { default as newYorkBedroom } from "./new-york-bedroom.png";
+export { default as newYorkCentralPark } from "./new-york-central-park.png";
+export { default as newYorkPreview } from "./new-york-preview.png";
+
+// Green House
+export { default as greenHousePreview } from "./green-house-preview.png";
+export { default as greenHouse } from "./green-house.png";
+
+// Backseat
+export { default as backseatPreview } from "./backseat-preview.png";
+export { default as backseat } from "./backseat.png";
+
+// Noise
+export { default as noisePreview } from "./noise-preview.png";
+export { default as white } from "./white.png";
+export { default as pink } from "./pink.png";
+export { default as brown } from "./brown.png";
+
+// Future
+export { default as futurePreview } from "./future-preview.png";
+export { default as futureDesk } from "./future-desk.png";
+export { default as futureBedroom } from "./future-bedroom.png";
+
+// Slow Garden
+export { default as slowGardenPreview } from "./slow-garden-preview.png";
+export { default as slowGarden } from "./slow-garden.png";
+
+// Library
+export { default as libraryPreview } from "./library-preview.png";
+export { default as library } from "./library.png";
+
+// Art room
+export { default as artRoom } from "./art-room.png";
+export { default as artRoomPreview } from "./art-room-preview.png";
+
+// Above the clouds
+export { default as plane } from "./plane.png";
+export { default as planePreview } from "./plane-preview.png";
+
+// In the Woods
+export { default as inthewoodsInside } from "./inthewoods-inside.png";
+export { default as inthewoodsOutside } from "./inthewoods-outside.png";
+export { default as inthewoodsPreview } from "./inthewoods-preview.png";
+
+// Lake House
+export { default as lakeHousePreview } from "./lakehouse-preview.png";
+export { default as lakeHouseOutside } from "./lakehouse-outside.png";
+export { default as lakeHouseInside } from "./lakehouse-inside.png";
+
+// Fuji
+export { default as fuji } from "./fuji.png";
+export { default as fujiPreview } from "./fuji-preview.png";
+
+// Floating
+export { default as floatingPreview } from "./floating-preview.png";
+export { default as floating } from "./floating.png";
+
+// Sea Side
+export { default as seaSideInside } from "./seaside-inside.png";
+export { default as seaSideOutside } from "./seaside-outside.png";
+export { default as seaSidePreview } from "./seaside-preview.png";
+
+// Sunset Camping
+export { default as sunsetCamping } from "./sunset-camping.png";
+export { default as sunsetCampingPreview } from "./sunset-camping-preview.png";
+
+// Tokyo Night
+export { default as tokyoNightAlley } from "./tokyo-alley.png";
+export { default as tokyoNightRamenShop } from "./ramen-shop.png";
+export { default as tokyoNightPreview } from "./tokyo-night-preview.png";
+
+// Tree House
+export { default as treeHouse } from "./tree-house.png";
+export { default as treeHousePreview } from "./tree-house-preview.png";
+
+// Winter Night
+export { default as winterNight } from "./winterNight.png";
+export { default as winterNightPreview } from "./winterNightPreview.png";
+
+// Cozy Studio
+export { default as cozyStudio } from "./cozyStudio.png";
+export { default as cozyStudioPreview } from "./cozyStudioPreview.png";
+
+// Dreamy Forest
+export { default as dreamyForest } from "./dreamyForest.png";
+export { default as dreamyForestPreview } from "./dreamyForestPreview.png";
+
+// Seoul
+export { default as seoulInside } from "./seoul-inside.png";
+export { default as seoulOutside } from "./seoul-outside.png";
+export { default as seoulPreview } from "./seoul-preview.png";
---
+++ src/common/constants/index.ts
@@ -0,0 +1,13 @@
+/**
+ * TODO: Refactor this variable name, since we're using my mirror instead.
+ */
+export const digitalOcean = {
+  EFFECTS_ROOT_URL: "https://lofi-co-assets.vexcited.com/effects/",
+  ALARMS_ROOT_URL: "https://lofi-co-assets.vexcited.com/alarms/",
+  TRACKS_ROOT_URL: "https://lofi-co-assets.vexcited.com/ogtracks/",
+  SCENES_ROOT_URL: "https://lofi-co-assets.vexcited.com/scenes/",
+  WALLPAPERS_ROOT_URL: "https://lofi-co-assets.vexcited.com/wallpapers/",
+
+  /** NOTE: Should NOT be used in the app, since not supported. */
+  USER_FILES_ROOT_URL: "https://lofico.nyc3.digitaloceanspaces.com",
+} as const;
